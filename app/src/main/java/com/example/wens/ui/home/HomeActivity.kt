cfbb1f0fcae9a829c6108896a8ace748d9a5f9b6
surazverma <surazverma@gmail.com>
2020-12-09 22:18:53 +0530

    app : Home Activity creation

--- app/src/main/java/com/example/wens/ui/home/HomeActivity.kt
+++ app/src/main/java/com/example/wens/ui/home/HomeActivity.kt
@@ -1,11 +1,12 @@
-package com.example.wens
+package com.example.wens.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.wens.R
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
     }
 }
