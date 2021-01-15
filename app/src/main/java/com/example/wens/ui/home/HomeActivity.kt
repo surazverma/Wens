3ab7e52d86126db8e090eef797eb5079e5afe817
surazverma <surazverma@gmail.com>
2021-01-15 21:09:33 +0530

    app : nav component setup with bottom navigation graph

--- app/src/main/java/com/example/wens/ui/home/HomeActivity.kt
+++ app/src/main/java/com/example/wens/ui/home/HomeActivity.kt
@@ -1,30 +1,29 @@
 package com.example.wens.ui.home
 
 import android.os.Bundle
-import android.widget.TextView
-import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.wens.R
 import com.example.wens.model.objects.Articles
 import com.example.wens.util.ResultWrapper
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_home.*
 
 @AndroidEntryPoint
 class HomeActivity : AppCompatActivity() {
 
-
-    lateinit var tvTest: TextView
-
     private val  mHomeViewModel : HomeViewModel by viewModels()
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        setupViews()
 
-        tvTest = findViewById(R.id.testText)
+
         setObservers()
         mHomeViewModel.getTopHeadlinesFromC("us")
     }
@@ -33,32 +32,14 @@ class HomeActivity : AppCompatActivity() {
         mHomeViewModel.news
             .observe(this, Observer<ResultWrapper<List<Articles>>>
             {
-                when (it) {
-                    is ResultWrapper.Loading -> {
-                        showToast("Loading....")
-                        tvTest.text = "Loading..."
-                    }
-                    is ResultWrapper.Success -> {
-                        showToast("Success")
-                        tvTest.text = it.value[0].title
-                    }
-                    is ResultWrapper.NetworkError -> {
-                        showToast("Error...")
-                        tvTest.text = it.error
-                    }
-                    is ResultWrapper.GenericError -> {
-                        showToast("Error...")
-                        tvTest.text = it.error
-                    }
-                    is ResultWrapper.ServerError -> {
-                        showToast("Error...")
-                        tvTest.text = it.error
-                    }
-                }
+
             })
     }
 
-
-    private fun showToast(message: String): Toast = Toast.makeText(this, message, Toast.LENGTH_SHORT)
+    fun setupViews(){
+        val navController = findNavController(R.id.nav_host_fragment)
+        bottom_nav.setupWithNavController(navController)
+        setupActionBarWithNavController(navController)
+    }
 
 }
